9e651c0b6e74d6e14db0bde3c018ba8c8af622a1
d-issy <12374694+d-issy@users.noreply.github.com>
2022-10-16 10:09:56 +0900

    python311 new feature

---
+++ lang-python/versions/3.11/test_ExceptionGroup.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class TestExceptionGroup(unittest.TestCase):
+    def test_group(self):
+        try:
+            raise ExceptionGroup("examples", [ValueError(), TypeError()])
+        except ExceptionGroup as eg:
+            assert type(eg.exceptions[0]) is ValueError
+            assert type(eg.exceptions[1]) is TypeError
+
+    # mypy not supported syntax yet
+
+    # def test_group2(self):
+    #     try:
+    #         raise ExceptionGroup("examples", [ValueError(), TypeError()])
+    #     except* ValueError:
+    #         print("ValueError")
+    #     except* TypeError:
+    #         print("TypeError")
---
+++ lang-python/versions/3.11/test_tomllib.py
@@ -0,0 +1,24 @@
+import tomllib
+import unittest
+
+
+class TestToml(unittest.TestCase):
+    def test_toml(self):
+        toml = """\
+[tool.poetry]
+name = "python311-test"
+version = "0.1.0"
+description = ""
+authors = ["issy"]
+
+[tool.poetry.dependencies]
+python = "^3.11"
+"""
+
+        data = tomllib.loads(toml)
+        assert data["tool"]["poetry"]["name"] == "python311-test"
+        assert data["tool"]["poetry"]["version"] == "0.1.0"
+        assert data["tool"]["poetry"]["description"] == ""
+        assert len(data["tool"]["poetry"]["authors"]) == 1
+
+        assert data["tool"]["poetry"]["dependencies"]["python"] == "^3.11"
